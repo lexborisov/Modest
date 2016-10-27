4a6c971fe87880b97bcf7f69d90020608915b23d
lexborisov <lex.borisov@gmail.com>
2016-10-28 01:41:16 +0300

    Makefile fix

--- source/myhtml/Makefile.mk
+++ source/myhtml/Makefile.mk
@@ -1,11 +1,13 @@
 find_files_h = $(wildcard $(SRCDIR)/myhtml/$(dir)/*.h)
 find_files_c = $(wildcard $(SRCDIR)/myhtml/$(dir)/*.c)
 
-SUBDIRS := . "utils"
+SUBDIRS := . utils
 HDRS += $(foreach dir,$(SUBDIRS),$(find_files_h))
 SRCS += $(foreach dir,$(SUBDIRS),$(find_files_c))
 
 myhtml_clone: MyHTML_DIR_$(SUBDIRS)
+	mkdir -p $(INCLUDE_TMP)/myhtml/utils
+	cp $(SRCDIR)/myhtml/utils/*.h $(INCLUDE_TMP)/myhtml/utils
 
 MyHTML_DIR_$(SUBDIRS):
 	mkdir -p $(INCLUDE_TMP)/myhtml/$(patsubst MyHTML_DIR_%,%,$@)
