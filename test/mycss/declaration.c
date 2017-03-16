4b7a55480fb2047991d84b394e3173791547a519
lexborisov <lex.borisov@gmail.com>
2017-03-16 20:06:31 +0300

    Fixed serialization html node

--- test/mycss/declaration.c
+++ test/mycss/declaration.c
@@ -340,7 +340,7 @@ mycore_string_raw_t test_process_result_from_node(test_data_t *test_data, myhtml
 {
     mycore_string_raw_t str = {0};
     
-    if(myhtml_serialization_node_buffer(node->child, &str) == false) {
+    if(myhtml_serialization_node_buffer(node->child, &str)) {
         fprintf(stderr, "Could serialization HTML node\n");
         exit(EXIT_FAILURE);
     }
